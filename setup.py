0f05bc1f0698f13d4128e32933880663ff89f60a
danielpdwalker <danielpdwalker@gmail.com>
2023-02-14 10:47:52 +0000

    Added new elt.sh and channel file to setup.py. Bumped the version

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name="analyze-github",
-    version="0.1.0",
+    version="0.1.1",
     description="Meltano project file bundle of Matatika datasets for tap-github variant meltanolabs",
     packages=find_packages(),
     package_data={
         "bundle": [
             "analyze/datasets/tap-github/*.yml",
-            "analyze/channels/*.yml"
+            "analyze/channels/*.yml",
+            "orchestrate/tap-github/elt.sh"
         ]
     },
 )
