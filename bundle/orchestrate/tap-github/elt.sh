deb16a85f037ac17b3c74378a0b3a26c40529b33
ReubenFrankel <60552974+ReubenFrankel@users.noreply.github.com>
2024-09-20 13:13:50 +0100

    `elt` -> `run`

--- bundle/orchestrate/tap-github/elt.sh
+++ bundle/orchestrate/tap-github/elt.sh
@@ -3,8 +3,8 @@
 # Exit on error
 set -e
 
-# Run the elt, and dbt commands and tests
-meltano elt "$EXTRACTOR" "$LOADER" --transform=skip
+# Run the EL, and dbt commands and tests
+meltano run "$EXTRACTOR" "$LOADER"
 meltano invoke dbt deps
 meltano invoke dbt snapshot --select tap_github
 meltano invoke dbt run -m tap_github
